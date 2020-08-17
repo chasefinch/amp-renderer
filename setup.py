e7f6ab478058c9a67ee6d38286d2fcbf2a5a9554
Chase Finch <chase@finch.email>
2020-08-17 16:51:57 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="amp-renderer",
+    version="0.0.1",
+    author="Chase Finch",
+    author_email="chase@finch.email",
+    description="Unofficial Python port of server-side rendering from AMP Optimizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chasefinch/amp-renderer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
