f73032c306f700182740bd17d0b52b2fbeffc6f0
Chase Finch <chase@finch.email>
2020-08-26 10:50:27 -0400

    Bump version for 1.1.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='amp-renderer',
-    version='1.1.3',
+    version='1.1.4',
     author='Chase Finch',
     author_email='chase@finch.email',
     description='Unofficial Python port of server-side rendering from AMP Optimizer',
